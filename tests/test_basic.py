c3d6f07abf93a7ff522b9913abe2199154910e4a
Marco Heinemann <marco.heinemann@useblocks.com>
2022-10-12 14:51:49 +0200

    Fixed TC

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -9,7 +9,7 @@ from sphinx_emf.cli import run
 def test_run_main():
     """Check whether main function runs through."""
     dir_this_file = os.path.dirname(os.path.realpath(__file__))
-    config_path = os.path.join(dir_this_file, "data", "prod", "config_writer.py")
+    config_path = os.path.join(dir_this_file, "data", "prod", "config_sphinx_emf.py")
     runner = CliRunner()
     response = runner.invoke(run, [config_path])
     assert response.exit_code == 0
