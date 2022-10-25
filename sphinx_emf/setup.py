18298e3e5e606109b9c5a14be6878d6d541889e3
Marco Heinemann <marco.heinemann@useblocks.com>
2022-10-25 17:14:16 +0200

    Removed unused builder config

--- sphinx_emf/setup.py
+++ sphinx_emf/setup.py
@@ -17,18 +17,10 @@ def setup(app) -> Dict[str, Any]:  # noqa: F841  # used by Sphinx when registeri
 
     # configurations
     # types are not given as pydantic is used to check the config in detail (see config/model.py)
-    app.add_config_value(name="emf_path_m1_model", default=None, rebuild="html")
     app.add_config_value(name="emf_path_m2_model", default=None, rebuild="html")
-    app.add_config_value(name="emf_rst_indent", default=3, rebuild="html")
-    app.add_config_value(name="emf_allowed_classes", default=[], rebuild="html")
-    app.add_config_value(name="emf_denied_classes", default=[], rebuild="html")
-    app.add_config_value(name="emf_allowed_values", default={}, rebuild="html")
-    app.add_config_value(name="emf_denied_values", default={}, rebuild="html")
-    app.add_config_value(name="emf_remove_unlinked_types", default=[], rebuild="html")
-    app.add_config_value(name="emf_pre_read_hook", default=[], rebuild="html")
-    app.add_config_value(name="emf_post_read_hook", default=[], rebuild="html")
+    app.add_config_value(name="emf_pre_read_hook", default=None, rebuild="html")
+    app.add_config_value(name="emf_post_read_hook", default=None, rebuild="html")
     app.add_config_value(name="emf_class_2_need_def", default={}, rebuild="html")
-    app.add_config_value(name="emf_sort_field", default=None, rebuild="html")
     app.add_config_value(name="emf_model_roots", default=[], rebuild="html")
 
     return {
